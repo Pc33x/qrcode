fff28a10a78b5c1caa282af6e5b8a604a354d0dd
Pc33x <apceh4ka@gmail.com>
2025-01-15 18:33:11 +0100

    New README file and others staff

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+import qrcode
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello, world"
+
+    if request.method == "GET":
+        return render_template("index.html")
+    
+    if request.method == "POST":
+        input = request.form.get("user_text")
+        img = qrcode.make(input)
+
+        img.save("static/qr.png")
+
+        return render_template("qrcode.html")
